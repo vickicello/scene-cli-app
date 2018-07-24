0f4396a47416599539a906f887f21d6b34193c79
Vicki Hamm <vicki.cello@gmail.com>
2018-07-24 23:06:15 +0000

    Refined scraped css elements

--- lib/scene_concerts/scraper.rb
+++ lib/scene_concerts/scraper.rb
@@ -5,16 +5,17 @@ URL = "https://www.clevescene.com/cleveland/EventSearch?feature=Concert&narrowBy
   def self.scrape_concerts
     doc = Nokogiri::HTML(open(URL))
     doc.css(".EventListing").css(".clearfix").each do |concert|
-      name = concert.css(".EventListing").css(".clearfix").css("h3 a")[2].text.strip
-      price = concert.css(".EventListing").css(".clearfix").css(".price").text.strip
-      venue = concert.css(".EventListing").css(".clearfix").css(".listingLocation a").text.strip
-      url = concert.css(".EventListing").css(".clearfix").css("h3 a")[2].attribute("href").value
+      name = concert.css("h3 a")[2].text.strip
+      price = concert.css(".price").text.strip
+      venue = concert.css(".listingLocation a")[0].text 
+      url = concert.css("h3 a")[2].attribute("href").value
       Scene::Concert.new(name, price, venue, url)
   end
 
   def self.scrape_details(concert)
     url = concert.url
     doc = Nokogiri::HTML(open(url))
+
     # need to drill down more, not sure how, on 'time = '
     # time = concert.css(".EventListing").css(".clearfix").css(".listing").text.strip
     concert.time = doc.css("").text.strip
